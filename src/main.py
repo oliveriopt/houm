d8b5ad26e1f4360a9d4d9671395e1dd0646818f3
oliveriopt <oliver.pozo.m@gmail.com>
2022-07-31 22:29:51 -0400

    improve of the output with keyboard

--- src/main.py
+++ src/main.py
@@ -5,15 +5,16 @@ from src.utils.get_api import get_api_keyboard
 def text_results(first:int, second:float, third:float, fourth:float, fifth:float) -> None:
 
     print("¿Cuántas visitas se realizaron en total?")
-    print(first)
+    print("EN total Se realizaron " + str(first) + " visitas.")
     print("¿Cuál es el promedio de propiedades por propietario?")
-    print(second)
+    print("Existen " + str(second) + " propiedades por propietario.")
     print("¿Cuál era la temperatura promedio de todas las visitas que se realizó en la propiedad del propietario con ID 2?")
-    print(third)
+    print("La temperatura promedio de las visitas que se realizaron en la propiedad del ID 2 fue de " + str(third) +
+          " grados Fareneit.")
     print("¿Cuál es la temperatura promedio de las visitas para los días con lluvia?")
-    print(fourth)
+    print("La temperatura promedio de las visitas realizadas con lluvia fueron de " + str(fourth) + " grados Fareneit.")
     print("¿Cuál es la temperatura promedio para las visitas realizadas en la localidad de Suba?")
-    print(fifth)
+    print("La temperatura promedio para las visitas realizadas en la localidad de Suba fueron de " + str(fifth) + " grados Fareneit.")
 
 def main():
     first, second, third, fourth, fifth = None, None, None, None, None
